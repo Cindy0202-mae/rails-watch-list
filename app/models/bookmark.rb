306d8190e8071927e4f10f4d6d6f2393f831678a
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-01-11 13:35:17 +0900

    added model, validations, association and seeded data into the database

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "should have a unique bookmark for a given movie/list couple" }
+end
