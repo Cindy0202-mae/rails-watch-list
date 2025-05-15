e99efbb0553f565b59c4b9e8a8a9d5672abe5d90
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-05-15 14:09:47 +0900

    updated the UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,23 @@ import "controllers"
 import "bootstrap"
 import "@popperjs/core"
 
+import Typed from "typed.js";
+
+document.addEventListener("turbo:load", function () {
+  const typedText = document.querySelector("#typed-text");
+  if (typedText) {
+    new Typed("#typed-text", {
+      strings: [
+        "Plan your movie nights effortlessly.",
+        "Discover and manage your favorite movies.",
+        "Start organizing your watchlist today!",
+      ],
+      typeSpeed: 50,
+      backSpeed: 20,
+      loop: true,
+      showCursor: true,
+      cursorChar: "🪄",
+    });
+  }
+});
 // console.log("Bootstrap and Popper.js loaded")
